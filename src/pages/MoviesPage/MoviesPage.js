75bd30c0269a92bd9c3acb99dd1b65b18f482f64
Elena <e.mandrovska@gmail.com>
2021-08-07 16:49:26 +0300

    Update MoviesPage.js

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -2,50 +2,75 @@ import { useState, useEffect } from "react";
 import { useLocation, useHistory, useRouteMatch } from "react-router-dom";
 
 import { SearchForm } from "../../components/SearchForm/SearchForm";
-import { MovieList } from "../../components/MovieList/MovieList"
+import { MovieList } from "../../components/MovieList/MovieList";
 import { getSearchMovies } from "../../services/apiService";
+import { Spinner } from "../../components/Spinner/Spinner";
 
 export function MoviesPage() {
-    const [searchQuery, setSearchQuery] = useState("");
+    // const [searchQuery, setSearchQuery] = useState("");
     const [movies, setMovies] = useState([]);
-    const { isExact } = useRouteMatch();
+    const [status, setStatus] = useState("idle");
     const history = useHistory();
     const location = useLocation();
     const currentPage = Number(new URLSearchParams(location.search).get("page")) || 1;
+    const searchQuery = new URLSearchParams(location.search).get("query");
 
     useEffect(() => {
-    
-        async function getMovies() {
-        if (!isExact) {
-      history.push("/");
+        if (!searchQuery) { 
+        return
     }
+        async function getMovies() {
+            setStatus("pending");
         try {
             const data = await getSearchMovies(searchQuery, currentPage);
             const { results } = data;
             setMovies(results);
-      } catch (error) {
-      console.log(error);
+            setStatus("resolved");
+        }
+        catch (error) {
+            console.log(error);
+            setStatus("rejected");
       }
     }
     getMovies();
-  }, [searchQuery, currentPage, history, isExact])
+    }, [searchQuery, currentPage, history])
 
     const handleQueryChange = query => {
-    if (query === searchQuery) {
-    //   toast.info('Please, enter new query!');
-      return;
+        if (!query || query === searchQuery) {
+            //   toast.info('Please, enter new query!');
+            return;
+        }
+        setMovies([]);
+        history.push({
+        ...location,
+        search: `query=${query}`,
+        });
     }
-    resetState();
-    setSearchQuery(query)
-  };    
 
-    const resetState = () => {
-        setSearchQuery("");
-    };
-    return (
-        <>
-        <SearchForm onSearch={handleQueryChange} />
-        {movies && <MovieList movies={movies} />}
-        </>
-    )
+        if (status === "idle") {
+            return (
+                <SearchForm onSearch={handleQueryChange} />
+            )
+        }
+        if (status === "pending") {
+            return (
+                <>
+                <SearchForm onSearch={handleQueryChange} />
+                <Spinner />
+                </>
+            )
+        }
+        if (status === "resolved") {
+            return (
+                <>
+                <SearchForm onSearch={handleQueryChange} />
+                {movies && <MovieList movies={movies} />}
+                </>
+            )
+        }
+        if (status === "rejected") {
+            return (
+                <SearchForm onSearch={handleQueryChange} />
+            )
+        } 
 }
