988604ad2bc2c0d53880ac4693ddc3cbc8c80bba
Elena <e.mandrovska@gmail.com>
2021-09-03 17:28:11 +0300

    change date format + age (lib moments.js)

--- src/components/Actors/Actors.js
+++ src/components/Actors/Actors.js
@@ -1,14 +1,23 @@
 import { ActorBirthday, ActorBio, ActorContainer, ActorName, ActorPhoto, ActorPlace, ActorsInfoContainer, MoviesWithActor } from "./Actors.styled";
 import defaultImg from "../../images/default.png";
+import moment from "moment";
 
-export function Actor({name, place, birthday, bio, photo}) {
+
+export function Actor({ name, place, birthday, bio, photo }) {
+
+    const currentAge = moment().diff(birthday, 'years');
+    // const currentAge = moment(birthday).toNow()
+    // console.log(currentAge)  
+    const birth = moment(birthday).format("MMMM Do YYYY");
+    // console.log(birth)
+
     return (
         <ActorContainer>
             <ActorPhoto src={photo ? `https://image.tmdb.org/t/p/original${photo}` : defaultImg}/>
             <ActorsInfoContainer>
                 <ActorName>{name}</ActorName>
-                <ActorBirthday>Date of birth: {birthday}</ActorBirthday>
-                <ActorPlace>Place of birth: {place}</ActorPlace>
+                {birthday && <ActorBirthday>Date of birth: {birth} ({currentAge} years)</ActorBirthday>}
+                {place && <ActorPlace>Place of birth: {place}</ActorPlace>}
                 <ActorBio>{bio}</ActorBio>
                 <MoviesWithActor>Movie list</MoviesWithActor>
             </ActorsInfoContainer>
