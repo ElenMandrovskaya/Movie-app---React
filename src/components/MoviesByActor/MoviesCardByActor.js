ed5872d7437ad5a7b540caa7214926c6162d9089
Elena <e.mandrovska@gmail.com>
2021-09-07 21:56:14 +0300

    link from actorpage to moviedetails

--- src/components/MoviesByActor/MoviesCardByActor.js
+++ src/components/MoviesByActor/MoviesCardByActor.js
@@ -1,17 +1,26 @@
 import moment from "moment";
+import { Link, useLocation } from "react-router-dom";
+
 import { MovieCardByActor, MovieInfoByActor, MoviePosterByActor, MovieTitleByActor, MovieYearByActor } from "./MoviesByActor.styled";
 import defaultImg from "../../images/default.png";
 
 export function KnownAsCard({id, title, posterPath, release}) {
     const relYear = moment(release).format("YYYY");
+    const location = useLocation();
     return (
     <>
-        <MovieCardByActor>
-            <MoviePosterByActor src={posterPath ? `https://image.tmdb.org/t/p/original${posterPath}` : defaultImg}/>
-            <MovieInfoByActor>
-                    <MovieTitleByActor>{title}</MovieTitleByActor>
-                    <MovieYearByActor>{relYear}</MovieYearByActor>
-            </MovieInfoByActor>
+            <MovieCardByActor>
+                <Link
+                to={{
+                    pathname: `/movies/${id}`,
+                    state: { from: location },
+                }}>
+                    <MoviePosterByActor src={posterPath ? `https://image.tmdb.org/t/p/original${posterPath}` : defaultImg}/>
+                    <MovieInfoByActor>
+                        <MovieTitleByActor>{title}</MovieTitleByActor>
+                        <MovieYearByActor>{relYear}</MovieYearByActor>
+                    </MovieInfoByActor>
+                </Link>
         </MovieCardByActor>
     </>
     )
